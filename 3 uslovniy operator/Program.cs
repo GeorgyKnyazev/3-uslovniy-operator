3f06c655c929530860e4016824a7103520747d6d
Георгий Князев <saint_georgy@yahoo.com>
2022-06-01 12:34:00 +0300

    Добавьте файлы проекта.

---
+++ 3 uslovniy operator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = 5;
+            int zoomStep = 7;
+            
+            while (startingNumber < 100)
+            {
+                Console.Write(startingNumber + " ");
+                startingNumber += zoomStep;
+            }
+        }
+    }
+}
